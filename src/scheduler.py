65ab65329f856ac9a065b6674e97af6385543c74
Spidy <166608100+spidy-naik@users.noreply.github.com>
2025-04-23 03:30:05 +0530

    500

--- src/scheduler.py
+++ src/scheduler.py
@@ -4,4 +4,4 @@ from .scraper import fetch_latest_posts
 async def scheduler():
     while True:
         await fetch_latest_posts()
-        await asyncio.sleep(600)
+        await asyncio.sleep(500)
